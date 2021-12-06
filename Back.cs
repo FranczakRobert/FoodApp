6e0765485f2c3cc6949ec8b0596e9462abef9e57
RobertFranczak <94225378+RobertFranczak@users.noreply.github.com>
2021-12-06 10:57:31 +0100

    Add new COMPARE method

--- Back.cs
+++ Back.cs
@@ -6,13 +6,7 @@ namespace App
     public class Back
     {
         Products products = new Products();
-        int q = 0;
-        int w = 0;
-        int x = 0;
-        int r = 0;
-        int c = 0;
-        int a = 0;
-        int b = 0;
+        int parametr = 0; 
 
         public void TakeIngridients()
         {
@@ -31,7 +25,68 @@ namespace App
             }
         }
 
-        public void Show()
+        int a =0,b =0,c =0,d =0,e =0,f=0,g=0;
+        public void Compare(string nazwa, int parametr, List<string> lista, List<string> pasujace)
+        {
+            
+           foreach(var item in products.list_products)
+            {
+
+                for (int i = 0; i < lista.Count; i++)
+                {
+                    if(nazwa == "KURCZAK Z RYZEM")
+                    parametr = a;
+
+                    if(nazwa == "SPAGHETTI ALLA CARBONARA")
+                    parametr = b;
+
+                    if(nazwa == "MAKARON Z KURKAMI I BOCZKIEM")
+                    c = parametr;
+
+                    if( nazwa == "SPAGHETTI BOLOGNESE")
+                    d = parametr;
+
+                    if(nazwa == "WARZYWNE CURRY Z DYNIĄ, KALAFIOREM I CIECIERZYCĄ")
+                    e = parametr;
+
+                    if(nazwa == "SAŁATKA Z AWOKADO I PIECZONYCH BATATÓW")
+                    f = parametr;
+
+                    if(nazwa == "GRATIN Z BATATÓW")
+                    g = parametr;
+                    
+                    if(item == lista[i])
+                    {
+                        pasujace.Add(item);
+                        parametr++;
+                    }
+
+                    
+                }
+            if(parametr != 0)
+            {
+                System.Console.WriteLine($"Przepis na: {nazwa} ");
+                if(parametr == 1)
+                System.Console.WriteLine($"Znaleziono {parametr} pasujący składnik na {lista.Count}\n \n \n \n");
+                else if(parametr > 1 && parametr < 5)
+                System.Console.WriteLine($"Znaleziono {parametr} pasujące składniki na {lista.Count}\n \n \n \n");
+                else
+                System.Console.WriteLine($"Znaleziono {parametr} pasujących składników na {lista.Count}\n \n \n \n");
+                break;
+            }
+        }
+    }
+    public void ShowFood()
+    {
+        this.Compare("KURCZAK Z RYZEM", parametr , products.kurczakzryzem, products.pasujace_skladniki_kurczak);
+        this.Compare("SPAGHETTI ALLA CARBONARA", parametr , products.spaghettiCarbonara, products.pasujace_skladniki_spaghetti);
+        this.Compare("MAKARON Z KURKAMI I BOCZKIEM", parametr, products.makaron_z_kurkami, products.pasujace_skladniki_kurki);
+        this.Compare("SPAGHETTI BOLOGNESE", parametr , products.bolognese , products.pasujace_bolognese);
+        this.Compare("WARZYWNE CURRY Z DYNIĄ, KALAFIOREM I CIECIERZYCĄ", parametr , products.ciecierzyca, products.pasujace_ciecierzyca);
+        this.Compare("SAŁATKA Z AWOKADO I PIECZONYCH BATATÓW", parametr, products.avocado, products.pasujace_avocado);
+        this.Compare("GRATIN Z BATATÓW", parametr , products.batat, products.pasujace_batat);
+    }
+          public void Show()
         {  
             bool stop = false;
             while(stop == false)
@@ -43,7 +98,7 @@ namespace App
                 input =  odp0.ToLower();
                 if(input == "tak")
                 {
-                    if(q > 0)
+                    if(a > 0)
                     {
                         System.Console.Write("Pasujące składniki do potrawy KURCZAK Z RYZEM to: ");
                         for (int i = 0; i < products.pasujace_skladniki_kurczak.Count; i++)
@@ -53,7 +108,7 @@ namespace App
                         System.Console.WriteLine("");
                     }
 
-                    if(x > 0)
+                    if(b > 0)
                     {
                         System.Console.Write("Pasujące składniki do potrawy SPAGHETTI ALLA CARBONARA to: ");
                         for (int i = 0; i < products.pasujace_skladniki_spaghetti.Count; i++)
@@ -63,7 +118,7 @@ namespace App
                         System.Console.WriteLine("\n\n");
                     }
 
-                    if(w > 0)
+                    if(c > 0)
                     {
                         System.Console.Write("Pasujące składniki do potrawy MAKARON Z KURKAMI I BOCZKIEM to: ");
                         for (int i = 0; i < products.pasujace_skladniki_kurki.Count; i++)
@@ -72,7 +127,7 @@ namespace App
                         }
                         System.Console.WriteLine("\n\n");
                     }
-                    if(r > 0)
+                    if(d > 0)
                     {
                         System.Console.Write("Pasujące składniki do potrawy SPAGHETTI BOLOGNESE to: ");
                         for (int i = 0; i < products.pasujace_bolognese.Count; i++)
@@ -81,7 +136,7 @@ namespace App
                         }
                         System.Console.WriteLine("\n\n");
                     }
-                    if(c > 0)
+                    if(e > 0)
                     {
                         System.Console.WriteLine("Pasujące składniki do potrawy WARZYWNE CURRY Z DYNIĄ, KALAFIOREM I CIECIERZYCĄ to: ");
                         for (int i = 0; i < products.pasujace_ciecierzyca.Count; i++)
@@ -90,7 +145,7 @@ namespace App
                         }
                         System.Console.WriteLine("\n\n");
                     }
-                    if( a > 0)
+                    if( f > 0)
                     {
                         System.Console.WriteLine("Pasujące składniki do potrawy SAŁATKA Z AWOKADO I PIECZONYCH BATATÓW to: ");
                         for (int i = 0; i < products.pasujace_avocado.Count; i++)
@@ -99,7 +154,7 @@ namespace App
                         }
                         System.Console.WriteLine("\n\n");
                     }
-                    if( b > 0)
+                    if( g > 0)
                     {
                         System.Console.WriteLine("Pasujące składniki do potrawy GRATIN Z BATATÓW: ");
                         for (int i = 0; i < products.pasujace_batat.Count; i++)
@@ -118,165 +173,6 @@ namespace App
                     stop = false;
                 }
             }
-        }
-
-        public void Compare()
-        {
-            //Kurczak z ryzem
-            foreach(var item in products.list_products)
-            {
-                for (int i = 0; i < products.kurczakzryzem.Count; i++)
-                {
-                    if(item == products.kurczakzryzem[i])
-                    {
-                        products.pasujace_skladniki_kurczak.Add(item);
-                        q++;
-                    }
-                    
-                }
-                //Makaron z kurkami
-                for (int i = 0; i < products.makaron_z_kurkami.Count; i++)
-                {
-                    if(item == products.makaron_z_kurkami[i])
-                    {
-                        products.pasujace_skladniki_kurki.Add(item);
-                        w++;
-                    }
-                    
-                }
-                //Spaghetti Carbonare
-                for (int i = 0; i < products.spaghettiCarbonara.Count; i++)
-                {
-                    if(item == products.spaghettiCarbonara[i])
-                    {
-                        products.pasujace_skladniki_spaghetti.Add(item);
-                        x++;
-                    }
-                }
-                //Bolognese
-                for (int i = 0; i < products.bolognese.Count; i++)
-                {
-                    if(item == products.bolognese[i])
-                    {
-                        products.pasujace_bolognese.Add(item);
-                        r++;
-                    }
-                }  
-                //ciecierzyca
-                for (int i = 0; i < products.ciecierzyca.Count; i++)
-                {
-                    if(item == products.ciecierzyca[i])
-                    {
-                        products.pasujace_ciecierzyca.Add(item);
-                        c++;
-                    }
-                }
-                //Avocado
-                for (int i = 0; i < products.avocado.Count; i++)
-                {
-                    if(item == products.avocado[i])
-                    {
-                        products.pasujace_avocado.Add(item);
-                        a++;
-                    }
-                }
-                //Batat
-                for (int i = 0; i < products.batat.Count; i++)
-                {
-                    if(item == products.batat[i])
-                    {
-                        products.pasujace_batat.Add(item);
-                        b++;
-                    }
-                }
-
-            }
-
-            //Kurczak z ryzem
-            int kurczak = products.kurczakzryzem.Count;
-            if(q != 0)
-            {
-                System.Console.WriteLine("Przepis na: KURCZAK Z RYZEM ");
-                if(q == 1)
-                System.Console.WriteLine($"Znaleziono {q} pasujący składnik na {kurczak}\n \n \n \n");
-                else if(q > 1 && q < 5)
-                System.Console.WriteLine($"Znaleziono {q} pasujące składniki na {kurczak}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {q} pasujących składników na {kurczak}\n \n \n \n");
-            }
-            //Makaron z kurkami
-            int kurki = products.makaron_z_kurkami.Count;
-            if(w != 0)
-            {
-                System.Console.WriteLine("Przepis na: MAKARON Z KURKAMI I BOCZKIEM");
-                if(w == 1)
-                System.Console.WriteLine($"Znaleziono {w} pasujący składnik na {kurki -2}\n \n \n \n");
-                else if(w > 1 && w < 5)
-                System.Console.WriteLine($"Znaleziono {w} pasujące składniki na {kurki -2}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {w} pasujących składników na {kurki -2}\n \n \n \n");
-            }
-            //Spaghetti carbonare
-            int carbonare = products.spaghettiCarbonara.Count;
-            if(x != 0)
-            {
-                System.Console.WriteLine("Przepis na: SPAGHETTI ALLA CARBONARA");
-                if(x == 1)
-                System.Console.WriteLine($"Znaleziono {x} pasujący składnik na {carbonare -1}\n \n \n \n");
-                else if(x > 1 && x < 5)
-                System.Console.WriteLine($"Znaleziono {x} pasujące składniki na {carbonare -1}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {x} pasujących składników na {carbonare -1}\n \n \n \n");
-            }
-            //bolognese
-            int bolognese = products.bolognese.Count;
-            if(r != 0)
-            {
-                System.Console.WriteLine("Przepis na: SPAGHETTI BOLOGNESE");
-                if(r == 1)
-                System.Console.WriteLine($"Znaleziono {r} pasujący składnik na {bolognese -2}\n \n \n \n");
-                else if(r > 1 && r < 5)
-                System.Console.WriteLine($"Znaleziono {r} pasujące składniki na {bolognese -2}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {r} pasujących składników na {bolognese -2}\n \n \n \n");
-            }
-
-            //ciecierzyca
-            int ciecierzca = products.ciecierzyca.Count;
-            if(c != 0)
-            {
-                System.Console.WriteLine("Przepis na: WARZYWNE CURRY Z DYNIĄ, KALAFIOREM I CIECIERZYCĄ");
-                if(c == 1)
-                System.Console.WriteLine($"Znaleziono {c} pasujący składnik na {ciecierzca}\n \n \n \n");
-                else if(r > 1 && r < 5)
-                System.Console.WriteLine($"Znaleziono {c} pasujące składniki na {ciecierzca}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {c} pasujących składników na {ciecierzca}\n \n \n \n");
-            }
-            //Avocado
-            int avocado = products.avocado.Count;
-            if( a!= 0)
-            {
-                System.Console.WriteLine("Przepis na: SAŁATKA Z AWOKADO I PIECZONYCH BATATÓW");
-                if(a == 1)
-                System.Console.WriteLine($"Znaleziono {a} pasujący składnik na {avocado -2}\n \n \n \n");
-                else if(r > 1 && r < 5)
-                System.Console.WriteLine($"Znaleziono {a} pasujące składniki na {avocado -2}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {a} pasujących składników na {avocado -2}\n \n \n \n");
-            }
-            //Batat
-            int batat = products.batat.Count;
-            if(b != 0)
-            {
-                System.Console.WriteLine("Przepis na: GRATIN Z BATATÓW");
-                if(b == 1)
-                System.Console.WriteLine($"Znaleziono {b} pasujący składnik na {batat -4}\n \n \n \n");
-                else if(r > 1 && r < 5) 
-                System.Console.WriteLine($"Znaleziono {b} pasujące składniki na {batat -4}\n \n \n \n");
-                else
-                System.Console.WriteLine($"Znaleziono {b} pasujących składników na {batat -4}\n \n \n \n");
-            }
-        }
-    }
+        }    
+}
 }
--- Program.cs
+++ Program.cs
@@ -10,10 +10,10 @@ namespace App
             Products products = new Products();
             Back back = new Back();
             back.TakeIngridients();
-            back.Compare();
+            back.ShowFood();
             System.Console.WriteLine("Czy chcesz zobaczyc które z podanych przez Ciebie składników pasują do poszczególnych potraw?");
             back.Show();
-            System.Console.WriteLine("Napisz pierwszy człon (słowo) potrawy, aby wyświetlić jej przygotowanie:");
+            System.Console.WriteLine("Wybierz potrawe, aby wyświetlić jej przygotowanie:");
             string odp = Console.ReadLine();
             products.ShowRecipes(odp);
             
